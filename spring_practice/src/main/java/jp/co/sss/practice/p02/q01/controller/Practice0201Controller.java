f311128a4bbdbfed7c249b69727d89143edd265e
hikaruk <hikaruk@C010423221.zipangu.cac.co.jp>
2026-05-28 15:32:08 +0900

    spring

--- spring_practice/src/main/java/jp/co/sss/practice/p02/q01/controller/Practice0201Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p02/q01/controller/Practice0201Controller.java
@@ -1,5 +1,23 @@
 package jp.co.sss.practice.p02.q01.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class Practice0201Controller {
 
+	@RequestMapping(path = "/first")
+	public String melon() {
+		return "practice02/01/before_redirect";
+	}
+
+	@RequestMapping(path = "/second")
+	public String watermelon() {
+		return "practice02/01/after_redirect";
+	}
+
+	@RequestMapping(path = "/third")
+	public String gourd() {
+		return "practice02/01/after_redirect";
+	}
 }
