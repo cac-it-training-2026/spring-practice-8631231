d790d0bbf0fcfe7a55bfbd5e7b3659e0cc811a8d
hikaruk <hikaruk@C010423221.zipangu.cac.co.jp>
2026-05-31 01:59:18 +0900

    Spring

--- spring_practice/src/main/java/jp/co/sss/practice/p04/q01/controller/Practice0401Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p04/q01/controller/Practice0401Controller.java
@@ -1,5 +1,34 @@
 package jp.co.sss.practice.p04.q01.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.sss.practice.p04.q01.form.BmiForm;
+
+@Controller
 public class Practice0401Controller {
+
+	@RequestMapping(path = "/bmi/input", method = RequestMethod.GET)
+	public String melon() {
+		return "practice04/01/bmi_input";
+	}
+
+	@RequestMapping(path = "/bmi/result", method = RequestMethod.POST)
+	public String watermelon(BmiForm bmiForm, Model model) {
+
+		double bmiheight = bmiForm.getHeight() / 100;
+		double bmiheight2 = bmiheight * bmiheight;
+		double bmi = bmiForm.getWeight() / bmiheight2;
+		bmiForm.setBmi(bmi);
+
+		model.addAttribute("name", bmiForm.getName());
+		model.addAttribute("height", bmiForm.getHeight());
+		model.addAttribute("weight", bmiForm.getWeight());
+
+		model.addAttribute("bmi", bmiForm.getBmi());
+		return "practice04/01/bmi_result";
+	}
 
 }
--- spring_practice/src/main/java/jp/co/sss/practice/p04/q02/controller/Practice0402Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p04/q02/controller/Practice0402Controller.java
@@ -1,5 +1,19 @@
 package jp.co.sss.practice.p04.q02.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
 public class Practice0402Controller {
+
+	@RequestMapping(path = "/numguess/start", method = RequestMethod.GET)
+	public String melon(HttpSession session) {
+
+		Integer hitNumber = (int) (Math.floor(Math.random() * 9) + 1);
+		return "practice04/02/numguess_start";
+	}
 
 }
